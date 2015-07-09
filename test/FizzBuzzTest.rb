e523faf5a4a346816d0e41527a99aaf4a348912e
Daniele Megna <daniele.megna@nyxsolutions.it>
2015-07-09 17:31:32 +0200

    Now we work with 5 multiplies

--- test/FizzBuzzTest.rb
+++ test/FizzBuzzTest.rb
@@ -9,11 +9,23 @@ describe "FizzBuzz" do
     expect(fizzbuzz(3)).to eq("Fizz")
     expect(fizzbuzz(6)).to eq("Fizz")
     expect(fizzbuzz(12)).to eq("Fizz")
+    expect(fizzbuzz(33)).to eq("Fizz")
+  end
+
+  it "multiple of five should return Buzz" do
+    expect(fizzbuzz(5)).to eq("Buzz")
+    expect(fizzbuzz(10)).to eq("Buzz")
+    expect(fizzbuzz(20)).to eq("Buzz")
+    expect(fizzbuzz(85)).to eq("Buzz")
   end
 
   def fizzbuzz(n)
     if(n % 3 == 0)
       return "Fizz"
+    end
+
+    if(n % 5 == 0)
+      return "Buzz"
     end
 
     n.to_s
